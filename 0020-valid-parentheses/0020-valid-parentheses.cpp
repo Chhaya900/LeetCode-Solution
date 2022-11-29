79391cc04f15127ef4cfd509a082db6db95ef851
Chhaya Gupta <113857048+Chhaya900@users.noreply.github.com>
2022-11-29 22:27:19 +0530

    Time: 2 ms (56.70%), Space: 6.4 MB (51.16%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    
+    bool isOpen(char c) 
+    {
+        return (c == '[' || c == '{'|| c == '(');
+    }
+    bool isClose(char c)
+    {
+        return (c == ']' || c == '}'|| c == ')');
+    }
+    bool isPair(char open, char close)
+    {
+        return (open == '{' && close == '}'|| 
+            open == '(' && close == ')' || 
+            open == '[' && close == ']');
+    }
+    bool isValid(string s) 
+    {
+        stack<char> open;
+        
+        for (char c : s)
+        {
+            if (isOpen(c))
+                open.push(c);  
+            else
+            {
+                if (open.empty()) // ]{{}[()] is wrong
+                    return false;
+                else if (!isPair(open.top(), c)) // (} is wrong
+                    return false;
+                else // if they match, pop the open one.
+                    open.pop();
+            }
+        }
+        return open.empty(); // return false if still there is unmatched (, {, [
+    }    
+};
