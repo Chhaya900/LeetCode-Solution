c4b7531b56c509b579db959389e8031e388a1540
Chhaya Gupta <113857048+Chhaya900@users.noreply.github.com>
2022-12-02 00:38:21 +0530

    Time: 726 ms (97.95%), Space: 252.9 MB (96.22%) - LeetHub

--- 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.cpp
+++ 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.cpp
@@ -11,31 +11,29 @@
 class Solution {
 public:
     ListNode* mergeNodes(ListNode* head) {
-    
     ListNode * stop=head;
-            ListNode * farward =head->next;
-        while(farward!=NULL)
+        ListNode * farward=head->next;
+        while(farward !=NULL)
         {
-            if(farward->val!=0)
+            if(farward->val !=0)
             {
                 stop->val=stop->val + farward->val;
-               
             }
             else
             {
-                if(farward->next == NULL)
+                if(farward->next ==NULL)
                 {
                    stop->next=NULL; 
                 }
                 else
                 {
-                   stop->next=farward;   
+              stop->next =farward;
                 }
-             stop=farward; 
+                stop = farward;
             }
-            
             farward=farward->next;
         }
+        
         return head;
     }
 };
